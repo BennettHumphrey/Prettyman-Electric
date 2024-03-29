98b8e9c68966162a1fbbd3397f2ba0427acfd09f
Ben Humphrey <bennetthumphrey98@gmail.com>
2024-03-29 15:33:20 -0500

    Add .env

--- Sanity/sanity.js
+++ Sanity/sanity.js
@@ -1,9 +1,9 @@
 import { createClient } from 'https://esm.sh/@sanity/client' 
 
-// TODO: hide projectId in .env
+const sanityProjectId = import.meta.env.VITE_SANITY_PROJECT_ID;
 
 export const sanityClient = createClient({
-  projectId: '50mhnpl5',
+  projectId: sanityProjectId,
   dataset: 'production',
   useCdn: true, // set to `false` to bypass the edge cache
   apiVersion: '2024-03-27', // use current date (YYYY-MM-DD) to target the latest API version
